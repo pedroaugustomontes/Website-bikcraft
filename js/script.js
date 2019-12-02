d1a71f04802d467ef4c8514fb82e63abe4d25709
PedroAugusto <nerdindiano@gmail.com>
2019-12-02 21:09:01 -0200

    Implementado animações na página PORTIFOLIO usando JS

---
+++ js/script.js
@@ -0,0 +1,16 @@
+if (window.SimpleSlide) {
+
+  new SimpleSlide({
+    slide: "quote", // nome do atributo data-slide="principal"
+    auto: true, // se o slide deve passar automaticamente
+    time: 5000, // tempo de transição dos slides
+  });
+
+  new SimpleSlide({
+    slide: "portifolio", // nome do atributo data-slide="principal"
+    auto: true, // se o slide deve passar automaticamente
+    time: 5000, // tempo de transição dos slides
+    nav: true,
+  });
+
+}
