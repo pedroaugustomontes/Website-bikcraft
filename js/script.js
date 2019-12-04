6a068956aa0c05a938719ecc92039fbf360926fb
PedroAugusto <nerdindiano@gmail.com>
2019-12-03 21:25:09 -0300

    Foi implementado animações em todo o site e animações de slite na página portfólio

--- js/script.js
+++ js/script.js
@@ -4,13 +4,21 @@ if (window.SimpleSlide) {
     slide: "quote", // nome do atributo data-slide="principal"
     auto: true, // se o slide deve passar automaticamente
     time: 5000, // tempo de transição dos slides
+    pauseOnHover: true // pausa a transição automática
   });
 
   new SimpleSlide({
     slide: "portifolio", // nome do atributo data-slide="principal"
     auto: true, // se o slide deve passar automaticamente
     time: 5000, // tempo de transição dos slides
-    nav: true,
+    nav: true, // se deve ou não mostrar a navegação
+    // pauseOnHover: true // pausa a transição automática
   });
+
+}
+
+if (window.SimpleAnime) {
+
+  new SimpleAnime();
 
 }
